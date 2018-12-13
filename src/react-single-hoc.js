a138116a3380ec26dd8beed3d5601a7e42922658
artalar <artalar@ya.ru>
2018-12-13 21:04:35 +0300

    improve(core): throw error if use hooks after initial phase

--- src/react-single-hoc.js
+++ src/react-single-hoc.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
 function preventUseHooksInRender() {
-  throw new Error("You can not use hooks outside initial phase");
+  throw new Error('You can not use hooks outside initial phase');
 }
 
 const context = React.createContext({});
@@ -20,20 +20,24 @@ export function withHooks(renderCreator) {
       this._subscribers = [];
       this._unsubscribers = [];
       this.state = {};
+
       const stateUpdaters = [];
       const stateUpdateSubscribers = [];
       let stateObserverId = 0;
+      let initialPhase = true;
+
       function updateState(state) {
         try {
           const newState = stateUpdaters.reduce(
             (acc, update) => Object.assign(acc, update(acc)),
-            Object.assign({}, state)
+            Object.assign({}, state),
           );
           return newState;
         } finally {
           stateUpdaters.length = 0;
         }
       }
+
       function publishUpdates(state) {
         try {
           stateUpdateSubscribers.forEach(cb => cb(state));
@@ -41,40 +45,49 @@ export function withHooks(renderCreator) {
           stateUpdateSubscribers.length = 0;
         }
       }
+
       const startUpdateState = () => {
         this.setState(updateState, publishUpdates);
       };
 
       const use = {
-        returnInitial: () => ({ props, context }),
+        returnInitial: () => {
+          if (!initialPhase) preventUseHooksInRender();
+          return { props, context };
+        },
         state: initialState => {
+          if (!initialPhase) preventUseHooksInRender();
           const id = stateObserverId++;
           this.state[id] = initialState;
           return {
             get: () => this.state[id],
             set: (update, cb) => {
               const updateCallback =
-                typeof update === "function" ? update : () => update;
+                typeof update === 'function' ? update : () => update;
               stateUpdaters.push(state => ({
-                [id]: updateCallback(state[id])
+                [id]: updateCallback(state[id]),
               }));
               if (cb) stateUpdateSubscribers.push(cb);
               startUpdateState();
-            }
+            },
           };
         },
         effect: cb => {
+          if (!initialPhase) preventUseHooksInRender();
           this._effects.push(cb);
         },
-        subscribe: subscriber => this._subscribers.push(subscriber)
+        subscribe: subscriber => {
+          if (!initialPhase) preventUseHooksInRender();
+          this._subscribers.push(subscriber);
+        },
       };
       this._render = renderCreator(use, props, context);
 
-      use.returnInitial = use.state = use.effect = use.subscribe = preventUseHooksInRender;
+      initialPhase = false;
     }
     componentDidMount() {
       this._unsubscribers = this._subscribers.map(cb =>
-        cb(this.props, this.context)
+        cb(this.props, this.context),
       );
       // TODO: this is really needed?
       this._effects.forEach(cb => cb(this.props, this.context));
