5d8857097322ff1c7e44092b7d861e733aac0063
artalar <artalar@ya.ru>
2018-12-13 22:10:46 +0300

    improve(build): remove peerDependencies from bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const PeerDepsExternalsPlugin = require('peer-deps-externals-webpack-plugin');
 
 module.exports = {
   mode: 'production',
@@ -26,6 +27,7 @@ module.exports = {
       },
     ],
   },
+  plugins: [new PeerDepsExternalsPlugin()],
   stats: {
     colors: true,
   },
